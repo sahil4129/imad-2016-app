df4d728a2e82abfb1ad500aad2a446bbb9847336
sahil4129 <s.g.navingoyal@gmail.com>
2016-09-25 10:00:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 var articles = {
     
     'article-one' : {
-    tittle:'Article one',
+    title:'Article one',
     heading :'Article one',
     date :' sep 5 ,2016',
     content :` <p>
@@ -25,7 +25,7 @@ var articles = {
            </p>`
 },
   'article-two' : {
-      tittle:'Article two',
+      title:'Article two',
     heading :'Article two',
     date :' sep 10,2016',
     content :` <p>
@@ -42,7 +42,7 @@ var articles = {
            </p>`
   },
   'article-three': {
-      tittle:'Article Three',
+      title:'Article Three',
     heading :'Article Three',
     date :' sep 15 ,2016',
     content :` <p>
@@ -61,16 +61,16 @@ var articles = {
 };
 
 function createtemplete (data){
-    var tittle = data.tittle;
+    var title = data.title;
     var heading = data.heading;
     var date = data.date;
     var content = data.content;
 var htmltemplete = ` 
 <html>
    <head>
-       <tittle>
-           ${tittle}
-       </tittle>
+       <title>
+           ${title}
+       </title>
            <link href="/ui/style.css" rel="stylesheet" />
    </head>
    <body>
