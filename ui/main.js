5e1115d9bc98c5edb57252337584e159d7dab2a5
sahil4129 <s.g.navingoyal@gmail.com>
2016-09-26 19:44:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+var element = document.getElementById('modi');
+var marginLeft =0;
+function moveRight ()
+{
+    marginLeft= marginLeft +10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function()
+{
+    var interval = setInterval (moveRight,100);
+};
