b0a3a553975701feb5d0b9db1ec0b299adca559d
sahil4129 <s.g.navingoyal@gmail.com>
2016-09-27 00:10:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,31 @@ var img = document.getElementById('modi');
 var marginLeft =0;
 function moveRight ()
 {
-    marginLeft= marginLeft +10;
+    marginLeft= marginLeft +1;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function()
 {
-    var interval = setInterval (moveRight,100);
+    var interval = setInterval (moveRight,10);
+};
+
+var counter =0;
+var button = document.getElementById('counter');
+button.onclick = function()
+{
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var counter=  request.responseText;
+                var spon = document.getElementById('count');
+                spon.innerhtml = counter.toString();
+            }
+        }
+        
+    };
+    request.open('GET',"http://sahil4129.imad.hasura-app",true);
+request.send(null);
 };
