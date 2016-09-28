998cc6e161de1c493b5ff23f48f71ee334c94c6c
sahil4129 <s.g.navingoyal@gmail.com>
2016-09-29 00:50:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,6 +29,6 @@ button.onclick = function()
         }
         
     };
-    request.open('GET',"http://sahil4129.imad.hasura-app.io",true);
+    request.open('GET','http://sahil4129.imad.hasura-app.io/counter',true);
 request.send(null);
 };
